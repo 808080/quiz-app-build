8d564d2da6276a153ebf01b4f5e5b15603b22c1f
Sergei Zaderaka <serzaderaka@gmail.com>
2020-06-07 23:07:18 +0300

    init

---
+++ precache-manifest.c706ddc8d9e86e074e1c498aba069e26.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb416119e4057083441134942d47ca56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11bad5f5b9e02dc7a783",
+    "url": "/static/css/main.9f227c04.chunk.css"
+  },
+  {
+    "revision": "7dc31bace997498a78d0",
+    "url": "/static/js/2.02de45e2.chunk.js"
+  },
+  {
+    "revision": "e6191615acbaa88870aef4cd0368e4ec",
+    "url": "/static/js/2.02de45e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11bad5f5b9e02dc7a783",
+    "url": "/static/js/main.8ff5b0bd.chunk.js"
+  },
+  {
+    "revision": "687ceba0ad95a48f724a",
+    "url": "/static/js/runtime-main.a76248a4.js"
+  },
+  {
+    "revision": "d0c132bab442491b8b9c11aea10f576a",
+    "url": "/static/media/bitmap.d0c132ba.png"
+  },
+  {
+    "revision": "5c8a4b1bb7d1a3ee0c02eaf1796c84d4",
+    "url": "/static/media/facebook-logo.5c8a4b1b.svg"
+  },
+  {
+    "revision": "43bd46f386c047e2f0f87c0e2b64c6cc",
+    "url": "/static/media/intro-bg.43bd46f3.png"
+  },
+  {
+    "revision": "1dacfa1b5741f40e93dcb05b54048ce9",
+    "url": "/static/media/next-arrow.1dacfa1b.svg"
+  },
+  {
+    "revision": "3c2f14af24a190f631df07333ab512d7",
+    "url": "/static/media/refresh-arrow.3c2f14af.svg"
+  },
+  {
+    "revision": "1a4736dfbe174323be7ff2f7a0688b85",
+    "url": "/static/media/result1.1a4736df.png"
+  },
+  {
+    "revision": "61f1ef1368e6d6e57c11a17ee8fd22a1",
+    "url": "/static/media/twitter-logo-silhouette.61f1ef13.svg"
+  },
+  {
+    "revision": "69b2e13e95c3ca70c349df068cf2ec89",
+    "url": "/static/media/vk-social-logotype.69b2e13e.svg"
+  }
+]);
