67cf02b5b445cd4879a09f51e26585c2d94f31e1
Sergei Zaderaka <serzaderaka@gmail.com>
2020-06-08 01:01:26 +0300

    share

---
+++ precache-manifest.6de28a5686a665970f2b212a9100761f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0ab9862b0cde745842d5fdc03c6137",
+    "url": "/quiz-app-build/index.html"
+  },
+  {
+    "revision": "c92ad422df47cf5c2ca0",
+    "url": "/quiz-app-build/static/css/main.9a8c8b6c.chunk.css"
+  },
+  {
+    "revision": "17bf6e8df6d22edc2fbd",
+    "url": "/quiz-app-build/static/js/2.78067e42.chunk.js"
+  },
+  {
+    "revision": "0ec1a01fb456549ee66e1bd8a420b18f",
+    "url": "/quiz-app-build/static/js/2.78067e42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c92ad422df47cf5c2ca0",
+    "url": "/quiz-app-build/static/js/main.b936aa0f.chunk.js"
+  },
+  {
+    "revision": "e4470f276839fc6feab8",
+    "url": "/quiz-app-build/static/js/runtime-main.8fe17f16.js"
+  },
+  {
+    "revision": "d0c132bab442491b8b9c11aea10f576a",
+    "url": "/quiz-app-build/static/media/bitmap.d0c132ba.png"
+  },
+  {
+    "revision": "5c8a4b1bb7d1a3ee0c02eaf1796c84d4",
+    "url": "/quiz-app-build/static/media/facebook-logo.5c8a4b1b.svg"
+  },
+  {
+    "revision": "43bd46f386c047e2f0f87c0e2b64c6cc",
+    "url": "/quiz-app-build/static/media/intro-bg.43bd46f3.png"
+  },
+  {
+    "revision": "1dacfa1b5741f40e93dcb05b54048ce9",
+    "url": "/quiz-app-build/static/media/next-arrow.1dacfa1b.svg"
+  },
+  {
+    "revision": "3c2f14af24a190f631df07333ab512d7",
+    "url": "/quiz-app-build/static/media/refresh-arrow.3c2f14af.svg"
+  },
+  {
+    "revision": "1a4736dfbe174323be7ff2f7a0688b85",
+    "url": "/quiz-app-build/static/media/result1.1a4736df.png"
+  },
+  {
+    "revision": "61f1ef1368e6d6e57c11a17ee8fd22a1",
+    "url": "/quiz-app-build/static/media/twitter-logo-silhouette.61f1ef13.svg"
+  },
+  {
+    "revision": "69b2e13e95c3ca70c349df068cf2ec89",
+    "url": "/quiz-app-build/static/media/vk-social-logotype.69b2e13e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c706ddc8d9e86e074e1c498aba069e26.js"
+  "/quiz-app-build/precache-manifest.6de28a5686a665970f2b212a9100761f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quiz-app-build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
